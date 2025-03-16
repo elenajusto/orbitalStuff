4515ed4b66aba11a9868bfdfd03bfd9269fb7c00
Elena Justo <intern@arlula.com>
2025-03-17 09:02:03 +1100

    Orbital mechanics stuff with Python

---
+++ geometry/main.py
@@ -0,0 +1,12 @@
+from astropy import units as u
+
+from poliastro.bodies import Earth, Mars, Sun
+from poliastro.twobody import Orbit
+
+# Data from Curtis, example 4.3
+r = [-6045, -3490, 2500] << u.km
+v = [-3.457, 6.618, 2.533] << u.km / u.s
+
+orb = Orbit.from_vectors(Earth, r, v)
+
+orb.plot()
